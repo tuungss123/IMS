274dab228b8bbcc4f6cac8a5b47fdc2bee6c6a8f
Brian Quicho <brian.quicho@hoolisoftware.com>
2024-01-18 20:20:05 +0800

    UI for Orders Page

--- FE/client-web/src/App.jsx
+++ FE/client-web/src/App.jsx
@@ -2,11 +2,13 @@ import React from "react"
 import SignIn from "./pages/sign-in"
 import HomePage from "./pages/home"
 import InventoryPage from "./pages/inventory"
+import OrdersPage from "./pages/orders"
 
 const App = () => {
   return (
     <>
-      <InventoryPage/>
+      <OrdersPage/>
+      
     </>
   )
 }
